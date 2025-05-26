eb411f6fd08fc174b6054983fc17904adea5cd71
Alex Shepeliev <shepeliev@users.noreply.github.com>
2025-05-26 15:17:31 +0300

    Make public API explicit (#178)

--- webrtc-kmp/src/jsAndWasmJsMain/kotlin/com/shepeliev/webrtckmp/externals/PlatformMediaStream.kt
+++ webrtc-kmp/src/jsAndWasmJsMain/kotlin/com/shepeliev/webrtckmp/externals/PlatformMediaStream.kt
@@ -3,12 +3,16 @@ package com.shepeliev.webrtckmp.externals
 import kotlin.js.JsName
 
 @JsName("MediaStream")
-external interface PlatformMediaStream {
-    val id: String
-    fun addTrack(track: PlatformMediaStreamTrack)
-    fun getTrackById(id: String): PlatformMediaStreamTrack?
-    fun removeTrack(track: PlatformMediaStreamTrack)
+public external interface PlatformMediaStream {
+    public val id: String
+
+    public fun addTrack(track: PlatformMediaStreamTrack)
+
+    public fun getTrackById(id: String): PlatformMediaStreamTrack?
+
+    public fun removeTrack(track: PlatformMediaStreamTrack)
 }
 
 internal expect fun PlatformMediaStream(): PlatformMediaStream
+
 internal expect fun PlatformMediaStream.getTracks(): List<PlatformMediaStreamTrack>
